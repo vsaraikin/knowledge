193aba95b217c91355ad13f2143c1fe1bbddddb3
vsaraikin <saraikinvladimir228boss@gmail.com>
2023-10-29 20:31:37 +0100

    vault backup 2023-10-29 20:30:57

--- script.sh
+++ script.sh
@@ -1,6 +1,17 @@
-# 
-### mm/dd/yyyy ###
-d=$(date +"%Y-%m-%d %T")
-git add .
-git commit -m "vault backup ${d}" 
-git push
+#!/bin/bash
+
+# Check for any unwanted files or changes
+git status
+
+# Prompt the user to continue
+read -p "Continue with commit? (y/n): " choice
+
+if [ "$choice" == "y" ] || [ "$choice" == "Y" ]; then
+    d=$(date +"%Y-%m-%d %T")
+    git add .  # Consider being more explicit here
+    git commit -m "vault backup ${d}" 
+    git gc
+    git push
+else
+    echo "Commit aborted."
+fi
